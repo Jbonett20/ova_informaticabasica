1f58f7eab05b2721e7a801259b1b70996db212b9
Jbonett <jbn19860220@gmail.com>
2025-09-10 08:03:26 -0500

    arreglos generales

--- assets/dist/js/contenido.js
+++ assets/dist/js/contenido.js
@@ -9,41 +9,42 @@ $(document).ready(function () {
   $("#PlayAudio").hide();
   $("#StopAudio").hide();
 });
+let maxVelocidad=1;
+let terminado =0;
 let contenidoActual = 1; // Índice inicial
 let totalDiapositivas = 6; // Número total de diapositivas
 // const archivo = `contenido${contenidoActual}.html`;
 function loadContent(containerId, url) {
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Error al cargar ${url}: ${response.statusText}`);
-      }
-      return response.text();
-    })
-    .then((data) => {
-      getProgreso();
-      document.getElementById(containerId).innerHTML = data;
-    })
-    .catch((error) => console.error(error));
+  document.getElementById(`${containerId}`).innerHTML = `
+    <iframe src="${url}" 
+            style="width:100%; height:600px; border:none;"></iframe>
+  `;
 }
 function cargarContenido(accion = null) {
   // Actualizar el índice según la acción
   if (accion === "siguiente" && contenidoActual < totalDiapositivas) {
     contenidoActual++;
+    reproducirAudio(contenidoActual)
     loadContent("contenido", `./contenido${contenidoActual}.html`);
-    reproducirAudio(contenidoActual);
+    getProgreso(); // <--- actualizar progreso
   } else if (accion === "anterior" && contenidoActual > 1) {
     contenidoActual--;
+    reproducirAudio(contenidoActual)
     loadContent("contenido", `./contenido${contenidoActual}.html`);
-    reproducirAudio(contenidoActual);
+    getProgreso(); // <--- actualizar progreso
   } else if (contenidoActual == totalDiapositivas) {
+    getProgreso(); // <--- actualizar al 100%
     Swal.fire({
       title: "Curso finalizado",
       text: "Usted ha llegado al final del curso, por favor presione el botón salir en la parte superior derecha",
       icon: "question",
     });
+       terminado=1;
+        let inputFinal = document.getElementById('finalizado');
+        inputFinal.value = "SI";
   }
 }
+
 function getProgreso() {
   const progreso = (contenidoActual / totalDiapositivas) * 100;
   const progressBar = document.querySelector("#progress-bar");
@@ -56,7 +57,7 @@ function pVideoDir() {
   $("#DivRecursos").addClass("activo");
   $("#DivRecursos").removeClass("inactivo");
   $("#VideoDirectivo").trigger("pause");
-  $('#vidIntroduc').trigger('play');
+  // $('#vidIntroduc').trigger('play');
 }
 function ReproVidDir(video) {
   $('#audiointro').trigger('pause');
@@ -71,10 +72,15 @@ function hideModalIntroduccion() {
   document.querySelectorAll(".btnNavegacion").forEach(function (btn) {
     btn.disabled = false; // Habilitar cada botón
   });
-  reproducirAudio(contenidoActual)
+
+  $("#StopAudio").hide();   
+  $("#PlayAudio").show();   
+  $("#velocidadAudio").show(); 
 }
+
 $("#StopAudio").click(function () {
   const audio = document.getElementById("audiointro");
+
   audio.pause();
   $("#StopAudio").hide();
   $("#PlayAudio").show();
@@ -82,15 +88,62 @@ $("#StopAudio").click(function () {
 
 $("#PlayAudio").click(function () {
   const audio = document.getElementById("audiointro");
+  audio.playbackRate =maxVelocidad;
   audio.play();
   $("#StopAudio").show();
   $("#PlayAudio").hide();
 });
 
 function reproducirAudio(indice) {
-  $('#audiointro').trigger('pause');
-  $("#StopAudio").show();
-  $("#PlayAudio").hide();
+
   $('#audiointro').attr('src', `./assets/audio/contenido${indice}.mp3`);
-  $('#audiointro').trigger('play');
+   $("#StopAudio").hide();
+  $("#PlayAudio").show();
 }
+// Control de velocidad de reproducción
+$(document).ready(function () {
+  const velocidades = [1, 1.25, 1.5, 2];
+  let indice = 0;
+
+  $("#velocidadAudio").click(function () {
+     const audio = document.getElementById("audiointro");
+    // avanzar índice
+    indice = (indice + 1) % velocidades.length;
+     maxVelocidad=velocidades[indice]
+     audio.playbackRate =maxVelocidad;
+    // poner texto en el botón
+    $(this).text(`${velocidades[indice]}x`);
+  });
+});
+
+$(document).ready(function () {
+  const audio = document.getElementById("audiointro");
+
+  // Cuando el audio termina, que quede en "pausa"
+  audio.addEventListener("ended", function () {
+    $("#StopAudio").hide();
+    $("#PlayAudio").show();
+    audio.pause(); // aseguramos que no quede en loop
+    audio.currentTime = 0; // opcional: reinicia al inicio
+  });
+});
+
+// Cuando cualquier modal se cierra, pausa todos los audios y videos
+$('.modal').on('hidden.bs.modal', function () {
+  // Pausar audios
+  const audio = document.getElementById("audiointro");
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0; // opcional: reinicia
+  }
+
+  // Pausar videos
+  $(this).find('video').each(function () {
+    this.pause();
+    this.currentTime = 0; // opcional: reinicia
+  });
+});
+$('#btn_salir').on('click',(e)=>{
+  e.preventDefault;
+  if(terminado==1)   completaCourse(1)
+})
